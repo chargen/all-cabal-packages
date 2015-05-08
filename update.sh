83e330b03a2f260e4d1cc0bb801441113624980c
Michael Snoyman <michael@snoyman.com>
2015-05-08 16:23:36 +0300

    basic debugging

--- update.sh
+++ update.sh
@@ -5,4 +5,6 @@ set -xue
 wget https://s3.amazonaws.com/stackage-travis/hackage-mirror/hackage-mirror.bz2
 bunzip2 hackage-mirror.bz2
 chmod +x hackage-mirror
+echo $AWS_ACCESS_KEY
+exit 1
 ./hackage-mirror --to s3://hackage.fpcomplete.com
