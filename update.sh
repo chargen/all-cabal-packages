185a35541b2f3ea10ed1bf5565566f3b03c39cb5
Michael Snoyman <michael@snoyman.com>
2015-05-08 16:30:52 +0300

    Are arguments revealed?

--- update.sh
+++ update.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 
-set -xue
+set -ue
 
 wget https://s3.amazonaws.com/stackage-travis/hackage-mirror/hackage-mirror.bz2
 bunzip2 hackage-mirror.bz2
 chmod +x hackage-mirror
-echo $AWS_ACCESS_KEY
-exit 1
-./hackage-mirror --to s3://hackage.fpcomplete.com
+./hackage-mirror --to s3://hackage.fpcomplete.com --access $AWS_ACCESS_KEY --secret $AWS_SECRET_KEY
